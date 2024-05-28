a8eeb3f94332bb50c813af26606f0575347a1300
Max Häberlein <haebma28@gmail.com>
2024-05-28 16:48:16 +0200

    merged

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 let slideIndex = 0;
-<<<<<<< HEAD
-showSlides();
-
-// Function to show the slides
-function showSlides() {
-=======
 var manually = 0;
 showSlides();
 
 // Function to show the slides
 function showSlides(c) {
->>>>>>> 991522b68eea7f03825b5dfa5292039f71d1d3bd
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
@@ -24,32 +17,22 @@ function showSlides(c) {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-<<<<<<< HEAD
-    setTimeout(showSlides, 4500); // Change image every 5 seconds
-=======
     if (!manually){
         setTimeout(showSlides, 4500); // call showSlides again after 4500ms
     }
->>>>>>> 991522b68eea7f03825b5dfa5292039f71d1d3bd
 }
 
 // Next/previous controls
 function plusSlides(n) {
     slideIndex += n - 1; // Adjust slideIndex to the correct position
-<<<<<<< HEAD
-=======
     if (slideIndex < 0) slideIndex = 0;
     manually = 1;
->>>>>>> 991522b68eea7f03825b5dfa5292039f71d1d3bd
     showSlides();
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
     slideIndex = n - 1; // Set slideIndex to the correct position
-<<<<<<< HEAD
-=======
     manually = 1;
->>>>>>> 991522b68eea7f03825b5dfa5292039f71d1d3bd
     showSlides();
 }
